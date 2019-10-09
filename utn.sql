c9f76f0a536554287757db42c4702d2879421773
julietAGuti <juliethaguti@gmail.com>
2019-10-08 22:02:55 -0300

    Add files via upload

--- utn.sql
+++ utn.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-04-2019 a las 03:05:11
--- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 5.6.28
+-- Tiempo de generación: 09-10-2019 a las 03:02:11
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,36 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `utn`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `alumnos`
---
-
-CREATE TABLE `alumnos` (
-  `Id` int(8) NOT NULL,
-  `Nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `Legajo` int(8) NOT NULL,
-  `Localidad` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `alumnos`
---
-
-INSERT INTO `alumnos` (`Id`, `Nombre`, `Legajo`, `Localidad`) VALUES
-(7, 'Pepe', 12, 1),
-(8, 'Jojo', 13, 2),
-(9, 'Maia', 14, 1),
-(11, 'Mario', 16, 3),
-(12, 'Marie', 16, 3),
-(13, 'Juan', 111, 1),
-(14, 'Maria', 112, 3),
-(15, 'Lucia', 113, 2),
-(16, 'Marcos', 114, 2),
-(17, 'marisa', 115, 1),
-(18, 'Jose', 116, 1);
 
 -- --------------------------------------------------------
 
@@ -78,54 +50,11 @@ INSERT INTO `envios` (`Numero`, `pNumero`, `Cantidad`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `localidad`
---
-
-CREATE TABLE `localidad` (
-  `Id` int(8) NOT NULL,
-  `Localidad` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `localidad`
---
-
-INSERT INTO `localidad` (`Id`, `Localidad`) VALUES
-(1, 'Avellaneda'),
-(2, 'Quilmes'),
-(3, 'Lanus');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `materias`
---
-
-CREATE TABLE `materias` (
-  `Id` int(8) NOT NULL,
-  `Nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `materias_alumnos`
---
-
-CREATE TABLE `materias_alumnos` (
-  `Id` int(8) NOT NULL,
-  `Id_Materia` int(6) NOT NULL,
-  `Id_Alumno` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `productos`
 --
 
 CREATE TABLE `productos` (
-  `pNumero` int(8) NOT NULL,
+  `pNumero` int(5) NOT NULL,
   `pNombre` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
   `Precio` float NOT NULL,
   `Tamaño` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
@@ -147,7 +76,7 @@ INSERT INTO `productos` (`pNumero`, `pNombre`, `Precio`, `Tamaño`) VALUES
 --
 
 CREATE TABLE `proveedores` (
-  `Numero` int(8) NOT NULL,
+  `Numero` int(5) NOT NULL,
   `Nombre` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
   `Domicilio` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
   `Localidad` varchar(80) COLLATE utf8_spanish2_ci NOT NULL
@@ -158,7 +87,7 @@ CREATE TABLE `proveedores` (
 --
 
 INSERT INTO `proveedores` (`Numero`, `Nombre`, `Domicilio`, `Localidad`) VALUES
-(100, 'Perez', 'Peron 876', 'Quilmes'),
+(100, 'Perez', 'Perón 876', 'Quilmes'),
 (101, 'Gimenez', 'Mitre 750', 'Avellaneda'),
 (102, 'Aguirre', 'Boedo 634', 'Bernal');
 
@@ -167,34 +96,10 @@ INSERT INTO `proveedores` (`Numero`, `Nombre`, `Domicilio`, `Localidad`) VALUES
 --
 
 --
--- Indices de la tabla `alumnos`
---
-ALTER TABLE `alumnos`
-  ADD PRIMARY KEY (`Id`);
-
---
 -- Indices de la tabla `envios`
 --
 ALTER TABLE `envios`
   ADD PRIMARY KEY (`Numero`,`pNumero`);
-
---
--- Indices de la tabla `localidad`
---
-ALTER TABLE `localidad`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `materias`
---
-ALTER TABLE `materias`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `materias_alumnos`
---
-ALTER TABLE `materias_alumnos`
-  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indices de la tabla `productos`
@@ -213,35 +118,18 @@ ALTER TABLE `proveedores`
 --
 
 --
--- AUTO_INCREMENT de la tabla `alumnos`
---
-ALTER TABLE `alumnos`
-  MODIFY `Id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
---
--- AUTO_INCREMENT de la tabla `localidad`
---
-ALTER TABLE `localidad`
-  MODIFY `Id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `materias`
---
-ALTER TABLE `materias`
-  MODIFY `Id` int(8) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `materias_alumnos`
---
-ALTER TABLE `materias_alumnos`
-  MODIFY `Id` int(8) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `pNumero` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `pNumero` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
-  MODIFY `Numero` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `Numero` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
